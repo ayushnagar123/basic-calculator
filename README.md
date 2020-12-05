d5e00c939e363bbf4ceccdb00ae50d3f0d3df299
Ayush Nagar <an431999@gmail.com>
2020-12-05 23:29:04 +0530

    basic calculator version 1.0

---
+++ README.md
@@ -0,0 +1,23 @@
+## What is this?
+
+calculate basic maths algebra.
+
+## Installation
+
+`npm i basic-calculator --save`
+
+## Operations
+```
+const calculator = require('basic-calculator')
+
+calculator(1,2,{
+    "operation":"add"
+})
+```
+
+## options
+- add :-to add two number
+- difference :-to substract two number
+- product :- tp multiply two numbers
+- divide :-to divide two numbers where 2nd number must not be 0
+- remainder :-to find remainder of 1st with respect to second where 2nd number must not be 0
---
+++ index.js
@@ -0,0 +1,22 @@
+function calculate(n1,n2,options){
+    if(options.operation=="sum"){
+        return n1+n2;
+    }
+    if(options.operation=="difference"){
+        return n1-n2;
+    }
+    if(options.operation=="product"){
+        return n1*n2;
+    }
+    if(n2!==0){
+        if(options.operation=="divide"){
+            return n1/n2;
+        }
+        if(options.operation=="remainder"){
+            return n1%n2;
+        }
+    }
+    return "invalid operation";
+}
+
+module.exports.calculate = calculate;
